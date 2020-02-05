4dbba10ece8d9043d5790d787ebdfe40bcdfd944
pengjielele <386276251@qq.com>
2020-02-05 16:16:34 +0800

    deploy 0.1

---
+++ app/vue.config.js
@@ -0,0 +1,13 @@
+const webpack = require("webpack");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: () => {
+          return "Last Build On " + new Date();
+        }
+      })
+    ]
+  }
+};
