4e2fc8fdfbc75a6d24902631dc9933ece1bff6f1
pengjielele <386276251@qq.com>
2020-04-17 21:59:52 +0800

    deploy

--- app/src/db/front/codeimgs.js
+++ app/src/db/front/codeimgs.js
@@ -3,150 +3,150 @@ export const codeimgs = [
     id: 1,
     title: "实现ajax",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/ajax.png"
+      "https://m.pengjielee.cn/img/ajax.png"
   },
   {
     id: 2,
     title: "实现ajax(版本2)",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/ajax2.png"
+      "https://m.pengjielee.cn/img/ajax2.png"
   },
   {
     id: 3,
     title: "实现call",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/call.png"
+      "https://m.pengjielee.cn/img/call.png"
   },
   {
     id: 4,
     title: "实现apply",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/apply.png"
+      "https://m.pengjielee.cn/img/apply.png"
   },
   {
     id: 5,
     title: "实现bind",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/bind.png"
+      "https://m.pengjielee.cn/img/bind.png"
   },
   {
     id: 6,
     title: "函数柯里化",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/curry.png"
+      "https://m.pengjielee.cn/img/curry.png"
   },
   {
     id: 7,
     title: "函数防抖",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/debounce.png"
+      "https://m.pengjielee.cn/img/debounce.png"
   },
   {
     id: 8,
     title: "函数节流",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/throttle.png"
+      "https://m.pengjielee.cn/img/throttle.png"
   },
   {
     id: 9,
     title: "实现深拷贝",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/deepclone.png"
+      "https://m.pengjielee.cn/img/deepclone.png"
   },
   {
     id: 10,
     title: "实现eventbus",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/eventbus.png"
+      "https://m.pengjielee.cn/img/eventbus.png"
   },
   {
     id: 11,
     title: "实现继承",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/extend.png"
+      "https://m.pengjielee.cn/img/extend.png"
   },
   {
     id: 12,
     title: "实现instaceof",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/instanceof.png"
+      "https://m.pengjielee.cn/img/instanceof.png"
   },
   {
     id: 13,
     title: "实现jsonp",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/jsonp.png"
+      "https://m.pengjielee.cn/img/jsonp.png"
   },
   {
     id: 14,
     title: "实现new",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/new.png"
+      "https://m.pengjielee.cn/img/new.png"
   },
   {
     id: 15,
     title: "实现reduce",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/reduce.png"
+      "https://m.pengjielee.cn/img/reduce.png"
   },
   {
     id: 16,
     title: "实现promise(基础版)",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/promise.png"
+      "https://m.pengjielee.cn/img/promise.png"
   },
   {
     id: 17,
     title: "实现promise(高级版)",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/promise2.png"
+      "https://m.pengjielee.cn/img/promise2.png"
   },
   {
     id: 18,
     title: "实现promise.all()",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/promise_all.png"
+      "https://m.pengjielee.cn/img/promise_all.png"
   },
   {
     id: 19,
     title: "实现promise串行",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/promise_queue.png"
+      "https://m.pengjielee.cn/img/promise_queue.png"
   },
   {
     id: 20,
     title: "缓存系统设计",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/cache.png"
+      "https://m.pengjielee.cn/img/cache.png"
   },
   {
     id: 21,
     title: "发布订阅模式",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/pubsub.png"
+      "https://m.pengjielee.cn/img/pubsub.png"
   },
   {
     id: 22,
     title: "生成随机数",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/random.png"
+      "https://m.pengjielee.cn/img/random.png"
   },
   {
     id: 23,
     title: "二分查找",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/binary_search.png"
+      "https://m.pengjielee.cn/img/binary_search.png"
   },
   {
     id: 24,
     title: "冒泡排序",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/bubble_sort.png"
+      "https://m.pengjielee.cn/img/bubble_sort.png"
   },
   {
     id: 25,
     title: "快速排序",
     image:
-      "https://raw.githubusercontent.com/pengjielee/gallery/master/codes/quick_sort.png"
+      "https://m.pengjielee.cn/img/quick_sort.png"
   }
 ];
