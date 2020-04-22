a9ae0317791764432b9d03e67d8f0ab948c612ff
pengjielele <386276251@qq.com>
2020-04-22 11:23:10 +0800

    deploy

--- app/src/views/code/Index.vue
+++ app/src/views/code/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="page-code">
     <div class="nav">
-      <router-link to="/code/javascript">javascript</router-link>
+      <router-link to="/code/javascript">javascript</router-link> |
+      <router-link to="/codes">code imgs</router-link>
     </div>
     <router-view></router-view>
   </div>
